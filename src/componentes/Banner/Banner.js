02a230e8a6275681ca6d8408c5cef503a90f4f4e
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-01-14 01:43:54 -0300

    modulo-01-banner-modf

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -3,10 +3,9 @@ import './Banner.css'
 function Banner() {
     return (
         <header className="banner">
-            <img src="/images/banner.png" alt="O banner principal da página" />
+            <img src="/imagens/banner.png" alt="O banner principal da página" />
         </header>
     )
 
 }
-
 export default Banner
