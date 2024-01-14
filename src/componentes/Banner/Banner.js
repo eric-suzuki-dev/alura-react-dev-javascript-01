852dd7261aaa10056b1140eef003e80bdd0b5b33
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-01-14 02:59:38 -0300

    modulo-01-finalizado

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,11 +1,10 @@
 import './Banner.css'
 
-function Banner() {
+export const Banner = () => {
+    //JSX
     return (
         <header className="banner">
-            <img src="/imagens/banner.png" alt="O banner principal da página" />
+            <img src="/imagens/banner.png" alt="O banner principal da página do Organo" />
         </header>
     )
-
 }
-export default Banner
